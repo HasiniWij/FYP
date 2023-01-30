51a485530e8b56743c4e207f6e1011952fa5d368
HasiniWij <61408819+HasiniWij@users.noreply.github.com>
2023-01-30 23:22:00 +0000

    add supervisor list ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 import StudentBookMeeting from './components/StudentBookMeeting';
 import StudentDashboard from './components/StudentDashboard';
 import RequestSupervision from './components/RequestSupervision';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import SupervisorsList from './components/SupervisorsList';
+import SignIn from './components/SignIn';
 
 const App = () => {
   return (
@@ -13,6 +16,8 @@ const App = () => {
       <Route path="/studentBookMeeting" exact component={StudentBookMeeting} />
       <Route path="/studentDashboard" exact component={StudentDashboard} />
       <Route path="/requestSupervision" component={RequestSupervision} />
+      <Route path="/signIn" component={SignIn} />
+      <Route path="/supervisorsList" component={SupervisorsList} />
     </Router>
   );
 }
---
+++ src/components/SupervisorsList.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Style.css';
+
+export default function RequestSupervision() {
+  
+  const supervisors = [
+    {
+      name:'Jon Doe',
+      interests:'Data science, Web development'
+    },
+    {
+      name:'Lavinia Handerson',
+      interests:'Database, IOT'
+    },
+    {
+      name:'Ben Ten',
+      interests:'Data science, Web development'
+    }
+  ];
+  
+  const supervisorCards = supervisors.map((supervisor) =>
+    <div class='offset-1 col-5 supervisorCard'>
+      <h6>Name: {supervisor.name}</h6>
+      <h6>Interested areas: {supervisor.interests} </h6>
+      <button class='primaryButton projectIdeaButton'>Go to project ideas</button>
+    </div>
+  );
+
+  return (
+    <div class="container">
+      <div class="d-flex justify-content-center title">
+        <h1> Supervisors List </h1>
+      </div>
+      <div class='row'>
+        {supervisorCards}
+      </div>
+      <div class='row marginTop'>
+        <div class='offset-5 col-2'>
+          <button class='secondaryButton'>Go Back</button>
+        </div>
+      </div>
+    </div>
+  );
+}
