6676009ab108774c7b35658b90724f90409d1230
HasiniWij <61408819+HasiniWij@users.noreply.github.com>
2023-04-19 00:14:29 +0100

    match students and supervisors with projects

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
+use App\Models\Project;
+use App\Models\ProjectArea;
+use App\Models\Skill;
 use App\Models\Student;
 use App\Models\Supervisor;
 use App\Models\User;
@@ -10,22 +14,97 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class AdminController extends Controller{
-    public function PythonScript(){
-        $process = new Process(['python', 'C:/Users/THOSHIBA/Documents/mentor-mentee/backend/app/Http/Controllers/teest.py'],
-        null,
-    ['SYSTEMROOT' => getenv('SYSTEMROOT'), 'PATH' => getenv("PATH")]
     
-    );
-        $process->run();
-        
+    public function __construct()
+    {
+        $this->middleware('auth.role:admin,supervisor');
+    }
+    public function PythonScript($data){
+        $fileName = 'C:/Users/THOSHIBA/Documents/mentor-mentee/backend/resources/calculateSimilarity.py';
+        $process = new Process(['python',$fileName ,json_encode($data)],null,
+                               ['SYSTEMROOT' => getenv('SYSTEMROOT'), 'PATH' => getenv("PATH")]
+                              );
+        $process->run();  
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
-
-        $data = $process->getOutput();
-        print json_encode($data);
+        return $process->getOutput();
     }
 
+    public function getStudents() {
+        $result = User::where('role', 'student')->get();
+        $students=array();
+
+        foreach ($result as $student) {
+        array_push($students,
+            array(
+                "name"=>$student->name,
+                "universityId"=>strtok($student->email, '@')
+            )
+        );
+        }
+        print json_encode($students);
+   }
+
+   public function matchWithProjects(){
+
+        $result = Project::all();
+
+        $userProjects = array();
+        $studentProjects = array();
+        $supervisorProjects = array();
+        $supervisorCapacities=array();
+        $matchedStudents=array();
+        $fullSupervisors=array();
+        $students=array();
+
+        foreach ($result as $item) {
+            if(isset($userProjects[$item['userId']])){
+                $userProjects[$item['userId']] .= ' '.$item['description'];
+            }
+            else $userProjects[$item['userId']] = $item['description'];
+        }
+        
+        foreach ($userProjects as $key=>$value) {
+            $user = User::where('id', $key)->first();
+            if($user['role']==='supervisor'){
+                array_push($supervisorProjects, $value);
+                $supervisor = Supervisor::where('supervisorId', $key)->first();
+                array_push($supervisorCapacities, array(
+                    'key'=>$key,
+                    'capacity'=>$supervisor['capacity'],
+                    'numOfStudents'=>0
+                    )
+                );
+            }
+            else{
+                array_push($studentProjects, $value);
+                array_push($students, $key);
+            }
+        }
+        $projects = array(
+            'supervisorProjects'=>$supervisorProjects,
+            'studentProjects'=>$studentProjects
+        ); 
+       
+        $output = $this->PythonScript($projects);
+        $scores=json_decode($output);
+        foreach($scores as $score){
+            if(!in_array($score->index[1],$matchedStudents) && !in_array($score->index[0],$fullSupervisors) ){
+                array_push($matchedStudents,$score->index[1]);
+                $supervisorCapacities[$score->index[0]]['numOfStudents'] ++;
+                if($supervisorCapacities[$score->index[0]]['numOfStudents']==$supervisorCapacities[$score->index[0]]['capacity']){
+                    array_push($fullSupervisors,$score->index[0]);
+                } 
+                $student = Student::where('studentId',$students[$score->index[1]])->first();
+                $student->supervisorId = $supervisorCapacities[$score->index[0]]['key'];             
+                $student->save();
+            }
+        }
+        return response()->json([
+            'status' => 'success'
+         ]);
+   }
     public function updateSupervisorCapacity(Request $request){
         $data = $request->all();
         $supervisor = Supervisor::find($data['id']);
@@ -34,5 +113,17 @@ class AdminController extends Controller{
         return response()->json([
             'status' => 'success'
          ]);
+    }
+    public function getAdminStatistics(){
+        $studentCount = Student::count();
+        $totalCapacity = Supervisor::sum('capacity');
+        return response()->json([
+            'studentCount' => $studentCount,
+            'totalCapacity' => $totalCapacity
+         ]);
+    }
+
+    public function match(){
+        $this->matchWithProjects();
     }
 }
--- backend/resources/calculateSimilarity.py
+++ backend/resources/calculateSimilarity.py
@@ -3,27 +3,26 @@ os.environ['HOME'] = 'C:/Python'
 
 import json
 import sys
-
-data= json.loads(sys.argv[1])
-sentences = []
-for sentence in data:
-    sentences.append(data[sentence])
-
+from operator import itemgetter
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('all-MiniLM-L6-v2')
+
+data= json.loads(sys.argv[1])
 #encode the sentences 
-embeddings = model.encode(sentences, convert_to_tensor=True)
+supervisorEmbeddings = model.encode(data['supervisorProjects'], convert_to_tensor=True)
+studentEmbeddings = model.encode(data['studentProjects'], convert_to_tensor=True)
 #compute the similarity scores
-cosine_scores = util.cos_sim(embeddings, embeddings)
+cosine_scores = util.cos_sim(supervisorEmbeddings, studentEmbeddings)
+# print(cosine_scores.size())
+size = cosine_scores.size()
 #compute/find the highest similarity scores
 pairs = []
-for i in range(len(cosine_scores)-1):
-    for j in range(i+1, len(cosine_scores)):
-        text = cosine_scores[i] [j]
-        pairs.append({'index': [i, j], 'score': text.item() })
-#sort the scores in decreasing order 
-pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
-# print(type(pairs))
-print(json.dumps(pairs))
+for supervisorIndex in range(size[0]):
+    for studentIndex in range(size[1]):
+        score = cosine_scores[supervisorIndex] [studentIndex]
+        pairs.append({'index': [supervisorIndex, studentIndex], 'score': score.item() })
+
+orderedScore = sorted(pairs, key=itemgetter('score'), reverse=True)
+print(json.dumps(orderedScore))
 
 
